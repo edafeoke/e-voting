eb3262a3b879b53181cbd2139af36c4eb0fd0038
Edafe Oke Great <greatedafeoke@gmail.com>
2023-08-24 02:02:40 +0100

    Setup folder structure

---
+++ app/controllers/user_controller.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, render_template
+
+user_bp = Blueprint('user', __name__)
+
+@user_bp.route('/login')
+def login():
+    return render_template('user/login.html')
+
+@user_bp.route('/register')
+def register():
+    return render_template('user/register.html')
